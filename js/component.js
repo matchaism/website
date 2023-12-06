7c47c97188d9a71c8492dd984b57e3501c48e8a3
matchaism <60777705+macchanism@users.noreply.github.com>
2023-12-06 23:26:49 +0900

    modified by ChatGPT

--- js/component.js
+++ js/component.js
@@ -1,33 +1,54 @@
-function IncludeWorkPage(rootdir, workname, simplecard){
+// 指定されたワーク名の作業ページを取得し、ルートディレクトリに基づいて表示する関数
+function IncludeWorkPage(rootdir, workname, simplecard) {
   $.ajax({
-    url: rootdir + "component/work/" + workname + ".html",
-    cache: false,
-    async: false,
-    dataType: "html",
-    success: function(html){
-      html = html.replace(/\{\$root\}/g, rootdir);
-      if(simplecard==true){
-        html = $(html).find('#simple-card').html();
+      url: rootdir + "component/work/" + workname + ".html",
+      cache: false,
+      async: false,
+      dataType: "html",
+      success: function (html) {
+          // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
+          html = html.replace(/\{\$root\}/g, rootdir);
+
+          // シンプルカード表示が指定されている場合、対応する要素のみ抽出
+          if (simplecard === true) {
+              html = $(html).find('#simple-card').html();
+          }
+
+          // HTMLをドキュメントに書き込む
+          document.write(html);
       }
-      document.write(html);
-    }
   });
 }
 
-function IncludeHeader(rootdir, currentpagetype){
+// ヘッダーコンポーネントを取得し、現在のページタイプに応じて表示する関数
+function IncludeHeader(rootdir, currentpagetype) {
   $.ajax({
-    url: rootdir + "component/header.html",
-    cache: false,
-    async: false,
-    dataType: "html",
-    success: function(html){
-      html = html.replace(/\{\$root\}/g, rootdir);
-      if(rootdir=="./"){
-        html = $(html).find('#navbar-'+currentpagetype).addClass("active").attr("aria-current", "page").attr("href", "#").end().prop('outerHTML');
-      }else{
-        html = $(html).find('#navbar-'+currentpagetype).addClass("active").attr("aria-current", "page").end().prop('outerHTML');
+      url: rootdir + "component/header.html",
+      cache: false,
+      async: false,
+      dataType: "html",
+      success: function (html) {
+          // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
+          html = html.replace(/\{\$root\}/g, rootdir);
+
+          // ルートディレクトリが "./" の場合とそれ以外の場合で処理を分岐
+          if (rootdir === "./") {
+              html = $(html).find('#navbar-' + currentpagetype)
+                  .addClass("active")
+                  .attr("aria-current", "page")
+                  .attr("href", "#")
+                  .end()
+                  .prop('outerHTML');
+          } else {
+              html = $(html).find('#navbar-' + currentpagetype)
+                  .addClass("active")
+                  .attr("aria-current", "page")
+                  .end()
+                  .prop('outerHTML');
+          }
+
+          // HTMLをドキュメントに書き込む
+          document.write(html);
       }
-      document.write(html);
-    }
   });
 }
