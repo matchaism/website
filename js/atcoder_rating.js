01448e6cb5aba79cd185f5ae033bbf6cc974ed8d
matchaism <60777705+matchaism@users.noreply.github.com>
2024-02-12 01:18:29 +0900

    update contest page (auto update atcoder rating)

--- js/atcoder_rating.js
+++ js/atcoder_rating.js
@@ -1,25 +1,42 @@
-$.getJSON('./data/atcoder.json', function(data) {
+function GetAtCoderRating(rootdir) {
 
-  // データの取得が成功した場合
-  try {
+  path = rootdir + 'data/atcoder.json';
+  lastNewRating = 799;
+  color = '茶色';
+  if (rootdir=='../') color = 'Brown';
 
-    // データの最後の要素からNewRatingを取得
-    var lastNewRating = data[data.length - 1].NewRating;
+  $.ajax({
+
+    url: path,
+    cache: false,
+    async: false,
+    dataType: "json"
+
+  }).done(function(data, status, xhr) {
+
+    // データの取得が成功した場合
+    try {
+
+      // データの最後の要素からNewRatingを取得
+      lastNewRating = data[data.length - 1].NewRating;
+      if (rootdir=='../') color = 'Brown';
+
+    } catch (error) {
+
+      // エラーハンドリング: データの取得や処理中にエラーが発生した場合
+      console.error('データ処理中にエラーが発生しました:', error);
+
+    }
+
+  }).fail(function(xhr, status, error) {
+
+    // エラーハンドリング: リクエスト自体が失敗した場合
+    console.error('atcoder.json の取得に失敗しました:', textStatus, errorThrown);
+
+  }).always(function(arg1, status, arg2) {
+
     // 取得したNewRatingを表示する要素にセット
-    $('#atcoder_raiting').html('<i class="bi bi-graph-up pe-1"></i>Rating ' + lastNewRating + ' (Max 799), 茶色');
+    $('#atcoder_raiting').html('<i class="bi bi-graph-up pe-1"></i>Rating ' + lastNewRating + ' (Max 799), ' + color);
 
-  } catch (error) {
-
-    // エラーハンドリング: データの取得や処理中にエラーが発生した場合
-    console.error('データ処理中にエラーが発生しました:', error);
-    $('#atcoder_raiting').html('<i class="bi bi-graph-up pe-1"></i>Rating 799 (Max 799), 茶色');
-
-  }
-
-}).fail(function(jqXHR, textStatus, errorThrown) {
-
-  // エラーハンドリング: リクエスト自体が失敗した場合
-  console.error('./data/atcoder.json の取得に失敗しました:', textStatus, errorThrown);
-  $('#atcoder_raiting').html('<i class="bi bi-graph-up pe-1"></i>Rating 799 (Max 799), 茶色');
-
-});
+  });
+}
--- js/component.js
+++ js/component.js
@@ -1,54 +1,49 @@
 // 指定されたワーク名の作業ページを取得し、ルートディレクトリに基づいて表示する関数
 function IncludeWorkPage(rootdir, subdir, workname, simplecard) {
   $.ajax({
-      url: rootdir + "component/" + subdir + workname + ".html",
-      cache: false,
-      async: false,
-      dataType: "html",
-      success: function (html) {
-          // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
-          html = html.replace(/\{\$root\}/g, rootdir);
 
-          // シンプルカード表示が指定されている場合、対応する要素のみ抽出
-          if (simplecard === true) {
-              html = $(html).find('.simple-card').html();
-          }
+    url: rootdir + "component/" + subdir + workname + ".html",
+    cache: false,
+    async: false,
+    dataType: "html",
 
-          // HTMLをドキュメントに書き込む
-          document.write(html);
-      }
+  }).done(function(html, status, xhr) {
+
+    // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
+    html = html.replace(/\{\$root\}/g, rootdir);
+
+    // シンプルカード表示が指定されている場合、対応する要素のみ抽出
+    if (simplecard === true) html = $(html).find('.simple-card').html();
+
+    // HTMLをドキュメントに書き込む
+    document.write(html);
+
   });
 }
 
 // ヘッダーコンポーネントを取得し、現在のページタイプに応じて表示する関数
 function IncludeHeader(rootdir, currentpagetype) {
   $.ajax({
-      url: rootdir + "component/header.html",
-      cache: false,
-      async: false,
-      dataType: "html",
-      success: function (html) {
-          // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
-          html = html.replace(/\{\$root\}/g, rootdir);
 
-          // ルートディレクトリが "./" の場合とそれ以外の場合で処理を分岐
-          if (rootdir === "./") {
-              html = $(html).find('#navbar-' + currentpagetype)
-                  .addClass("active")
-                  .attr("aria-current", "page")
-                  .attr("href", "#")
-                  .end()
-                  .prop('outerHTML');
-          } else {
-              html = $(html).find('#navbar-' + currentpagetype)
-                  .addClass("active")
-                  .attr("aria-current", "page")
-                  .end()
-                  .prop('outerHTML');
-          }
+    url: rootdir + "component/header.html",
+    cache: false,
+    async: false,
+    dataType: "html",
 
-          // HTMLをドキュメントに書き込む
-          document.write(html);
-      }
+  }).done(function(html, status, xhr) {
+
+    // ルートディレクトリのプレースホルダーを実際のルートディレクトリに置換
+    html = html.replace(/\{\$root\}/g, rootdir);
+
+    // ルートディレクトリが "./" の場合とそれ以外の場合で処理を分岐
+    if (rootdir === "./") {
+      html = $(html).find('#navbar-' + currentpagetype).addClass("active").attr("aria-current", "page").attr("href", "#").end().prop('outerHTML');
+    } else {
+      html = $(html).find('#navbar-' + currentpagetype).addClass("active").attr("aria-current", "page").end().prop('outerHTML');
+    }
+
+    // HTMLをドキュメントに書き込む
+    document.write(html);
+
   });
 }
